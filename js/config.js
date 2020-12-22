5e7ded56ef9fe3b94b83b1f8853828f6fcec1bf1
Stephen Dawes <stephendawes@Stephens-MacBook-Air.local>
2020-12-22 11:35:15 +0000

    Added userpool details

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_EpofZzF8n', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5e0100jcd1jr4t6r9irvuk1dg9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
